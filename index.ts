9ea5379d94944746755de344f2348903fcd9ce94
Nicolas Van-Duysen <nicolas.ssj.vdn@gmail.com>
2024-03-16 11:31:13 +0100

    init project

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { ActivityType, Client, GuildMember } from "discord.js";
+import * as dotenv from "dotenv"
+
+dotenv.config({path: `${__dirname}/.env`});
+
+const client = new Client({
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
+});
+
+if (!client) {
+    throw new Error('Error loading client !')
+}
+
+client.on("ready", () => {
+    client.user?.setPresence({
+        activities: [
+        {
+            name: 'coucou Alexis',
+            type: ActivityType.Playing,
+        },
+        ],
+        status: 'online'
+    });
+    console.log(`BOT activé !`);
+});
+
+client.login(process.env.CLIENT_TOKEN);
