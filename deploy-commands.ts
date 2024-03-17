967f316e9c242cb8b62868f640ee256555d15305
Nicolas Van-Duysen <nicolas.ssj.vdn@gmail.com>
2024-03-17 10:02:34 +0100

    feat(deploy) : Ajout du fichier d'actualisation des commandes

---
+++ deploy-commands.ts
@@ -0,0 +1,42 @@
+const { REST, Routes } = require('discord.js');
+const dotenv = require('dotenv')
+const path = require('path')
+const fs = require('fs')
+
+dotenv.config({path: `${__dirname}/.env`});
+
+const commands: string[] = [];
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(foldersPath);
+
+for (const file of commandFiles) {
+    const filePath = path.join(foldersPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON());
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(process.env.CLIENT_TOKEN!);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
